e4f15b1d76817b3008881c3def7312c69ed75408
David Albert <davidbalbert@gmail.com>
2015-06-02 16:23:43 -0400

    Basic example libpq program

--- main.c
+++ main.c
@@ -6,8 +6,66 @@
 
 #include <libpq-fe.h>
 
+char *prog_name;
+
+static void
+usage() {
+        fprintf(stderr, "usage: %s database-url\n", prog_name);
+        exit(1);
+}
+
+static void
+dieConn(PGconn *conn) {
+        fprintf(stderr, "%s: %s", prog_name, PQerrorMessage(conn));
+        PQfinish(conn);
+        exit(1);
+}
+
+static void
+dieResult(PGconn *conn, PGresult *res) {
+        fprintf(stderr, "%s: %s", prog_name, PQerrorMessage(conn));
+        PQclear(res);
+        PQfinish(conn);
+        exit(1);
+}
+
 int
 main(int argc, char *argv[]) {
-        printf("Hello, libgit2-postgres!\n");
+        PGconn *conn;
+        PGresult *res;
+        int i, j;
+
+        prog_name = argv[0];
+
+        if (argc < 2) {
+                usage();
+        }
+
+        conn = PQconnectdb(argv[1]);
+
+        if (PQstatus(conn) != CONNECTION_OK) {
+                dieConn(conn);
+        }
+
+        res = PQexec(conn, "SELECT * FROM objects");
+
+        if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+                dieResult(conn, res);
+        }
+
+        int nfields = PQnfields(res);
+
+        for (i = 0; i < nfields; i++) {
+                printf("%-15s", PQfname(res, i));
+        }
+        printf("\n");
+
+        for (i = 0; i < PQntuples(res); i++) {
+                for (j = 0; j < nfields; j++)
+                        printf("%-15s", PQgetvalue(res, i, j));
+                printf("\n");
+        }
+
+        PQfinish(conn);
         return 0;
 }
